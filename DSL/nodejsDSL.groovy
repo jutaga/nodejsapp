6bb8a39dcdf65d5dbf174450fb057f868f2f684e
Juan Pablo Tabares Gallego <93737918+jutaga@users.noreply.github.com>
2024-05-03 11:15:31 -0500

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/jutaga/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('jutaga')
+            node / gitConfigEmail('juancho9615@hotmail.com')
         }
     }
     triggers {
